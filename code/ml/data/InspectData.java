1a5585905451d7c0a75c3fbc10bf142983b1e994
Michaelcho1201 <112720776+Michaelcho1201@users.noreply.github.com>
2023-12-02 16:05:59 -0800

    Update InspectData.java

--- code/ml/data/InspectData.java
+++ code/ml/data/InspectData.java
@@ -3,8 +3,9 @@ package ml.data;
 public class InspectData {
 
 	public static void main(String[]args) {
-		String path = "data/wines.train";
-		DataSet data = new DataSet(path, 1);
+		String path = "data/suicide-test.train";
+		WordFilter filter = new WordFilter(path);
+		DataSet data = new DataSet(path, filter, 1);
 		for (int i = 0; i < data.getData().size(); i++) {
 			System.out.println(data.getData().get(i).getLabel());
 		}
